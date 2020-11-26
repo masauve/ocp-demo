4034624b926b6ff1620fe9436fc2592655cd6c72
Martin Sauve <msauve@redhat.com>
2020-11-26 09:04:45 -0500

    tmp commit

---
+++ 01-api/product-catalog-server/src/test/java/com/redhat/demo/ProductEndpointTest.java
@@ -0,0 +1,44 @@
+package com.redhat.demo;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+@QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
+public class ProductEndpointTest {
+
+    @Test
+    public void testProductEndpoint() {
+        given()
+          .when().get("/api/product")
+          .then()
+             .statusCode(200)
+             .body("size()", is(12));
+    }
+
+    @Test
+    public void testExistingProductEndpoint() {
+        given()
+          .pathParam("id", 1)
+          .when().get("/api/product/{id}")
+          .then()
+            .statusCode(200)
+            .body("id",equalTo(1));
+    }
+
+    @Test
+    public void testMissingProductEndpoint() {
+        given()
+          .pathParam("id", 99)
+          .when().get("/api/product/{id}")
+          .then()
+            .statusCode(404);
+    }
+}
